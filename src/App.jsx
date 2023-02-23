ee932cab4e2576920ea29cbf4566ecaa44538f19
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-02-22 20:24:37 -0600

    Created page title, styled components and updated img

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
 import { useState } from 'react'
+import styled from '@emotion/styled';
+import ImageCripto from'./img/imagen-criptos.png';
 
+const Container = styled.div `
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  } 
+`
+
+const Image = styled.img `
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1 `
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 42px;
+
+  &::after {
+    content: '';
+    width: 140px;
+    height: 6px;
+    background-color: #66a2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+
+  }
+
+`
 function App() {
   
   return (
-    <h1>desde app</h1>
+    <Container>
+      <Image
+        src={ImageCripto}
+        alt="image cripto"
+      />
+      <div>
+        <Heading>Track the real-time price of your favorite Criptocurrencies</Heading>
+      </div>
+    
+    </Container>
   );
 }
 

b8258d1c63664f85ef19b06fc0e72d82ee7e2593
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-02-24 21:34:54 -0500

    updated currencies

--- src/data/currencies.js
+++ src/data/currencies.js
@@ -1,6 +1,6 @@
 const currencies = [
     {id: 'USD', name: 'US Dollar'}, 
-    {id: 'URY', name: 'Uruguayan peso'},
+    {id: 'MXN', name: 'Mexican peso'},
     {id: 'EUR', name: 'Euro'},
     {id: 'GBP', name: 'British pound sterling'}
 ]
